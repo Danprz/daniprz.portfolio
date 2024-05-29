0c0bff2b2ce3c5d47828b3dfa073cc3b0aa54a39
Daniprz <danperezm93@gmail.com>
2024-05-29 09:22:00 +0200

    Mejorando los estilos creando el boton dark light

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,10 @@
-module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx}'],
-    darkMode: 'class',
-    theme: {
-        extend: {
-            
-        }
-    },
-    plugins: []
-}
+/** @type {import('tailwindcss').Config} */
 
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
